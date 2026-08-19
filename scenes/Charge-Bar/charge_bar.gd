24b4d1c34057ad4efb9a7bf6a60ff4302fecde64
Julian <sahaguj@pnw.edu>
2026-08-19 00:08:27 -0500

    Attach attack bar to player and merge code

--- scenes/Charge-Bar/charge_bar.gd
+++ scenes/Charge-Bar/charge_bar.gd
@@ -1,6 +1,4 @@
-class_name ChargeBar extends Node2D
-
-@onready var progress_bar: TextureProgressBar = %TextureProgressBar
+class_name ChargeBar extends TextureProgressBar
 
 ## Seconds of holding to fill the bar completely.
 @export var total_charge_time: float = 2.0
@@ -9,33 +7,21 @@ class_name ChargeBar extends Node2D
 
 var current_charge: float = 0.0
 
-func _process(delta: float) -> void: 
-	# NOTE: This will most likely move to player script, add Direction and Damage
-	if Input.is_action_pressed("charge_up"):
-		_charge_bar(delta)
-	elif Input.is_action_pressed("charge_down"):
-		_charge_bar(delta)
-	elif Input.is_action_pressed("charge_left"):
-		_charge_bar(delta)
-	elif Input.is_action_pressed("charge_right"):
-		_charge_bar(delta)
-	elif Input.is_action_just_released("charge_up") or Input.is_action_just_released("charge_down") or Input.is_action_just_released("charge_left") or Input.is_action_just_released("charge_right"):
-		_release_attack(_get_charged_segments())
-
 func _ready() -> void:
-	progress_bar.value = 0
+	self.value = 0
+
+func add_charge(delta: float) -> void:
+	var fill_rate: float = self.max_value / total_charge_time
+	current_charge = minf(current_charge + fill_rate * delta, self.max_value)
+	self.value = current_charge
 
 
-func _charge_bar(delta: float) -> void:
-	var fill_rate: float = progress_bar.max_value / total_charge_time
-	current_charge = minf(current_charge + fill_rate * delta, progress_bar.max_value)
-	progress_bar.value = current_charge
-
-func _get_charged_segments() -> int:
-	var per_segment: float = progress_bar.max_value / segment_count
+func get_charged_segments() -> int:
+	var per_segment: float = self.max_value / segment_count
 	return clampi(floori(current_charge / per_segment), 0, segment_count)
 
-func _release_attack(charge: int) -> void: 
+func release_charge() -> void:
+	var charge: int = get_charged_segments()
 	print("Charged %d segments" % charge)
 	current_charge = 0.0
-	progress_bar.value = 0.0
+	self.value = 0.0
--- scenes/Player/character_controller.gd
+++ scenes/Player/character_controller.gd
@@ -2,10 +2,17 @@ extends Node2D
 
 """This is the player controller, handling inputs for the charges"""
 
-enum ChargeDir {NONE, UP, DOWN, LEFT, RIGHT}
+signal attack_charging(delta: float)
+signal attack_released
 
+enum ChargeDir {NONE, UP, DOWN, LEFT, RIGHT}
 var charge_state: ChargeDir = ChargeDir.NONE #hands which direction the player is holding*
-var time_held: float = 0.0 #how much time a button has been held
+
+@onready var charge_bar: ChargeBar = %ChargeBar
+
+func _ready() -> void:
+	attack_charging.connect(charge_bar.add_charge)
+	attack_released.connect(charge_bar.release_charge)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -13,26 +20,22 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("charge_up"):
 		#handle charging up
 		charge_state = ChargeDir.UP
-		print("up")
-	if Input.is_action_pressed("charge_down"):
+		attack_charging.emit(delta)
+	elif Input.is_action_pressed("charge_down"):
 		charge_state = ChargeDir.DOWN
-		print("down")
-	if Input.is_action_pressed("charge_left"):
+		attack_charging.emit(delta)
+	elif Input.is_action_pressed("charge_left"):
 		charge_state = ChargeDir.LEFT
-		print("left")
-	if Input.is_action_pressed("charge_right"):
+		attack_charging.emit(delta)
+	elif Input.is_action_pressed("charge_right"):
 		charge_state = ChargeDir.RIGHT
-		print("right")
+		attack_charging.emit(delta)
 	
-	#timer
-	if charge_state != ChargeDir.NONE:
-		time_held += delta
-		
 	#reset everything when button is released 
 	if Input.is_action_just_released("charge_up") or Input.is_action_just_released("charge_down") \
 	or Input.is_action_just_released("charge_left") or Input.is_action_just_released("charge_right"):
 		"""Function for activating a charge"""
-		print("released ", time_held)
+		attack_released.emit()
 		charge_state = ChargeDir.NONE
-		time_held = 0.0
+		
 		
